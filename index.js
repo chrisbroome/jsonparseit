f8e445e07ae91e107cc6be2ab85723f26a2dc421
Christopher Broome <ChrisBroome7@gmail.com>
2014-10-28 22:59:01 -0400

    Implement a working parser

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 'use strict';
 var
+  util = require('util'),
   fs = require('fs'),
+  _ = require('lodash'),
   Tokenizer = require('./lib/tokenizer'),
+  Parser = require('./lib/parser'),
   filename = process.argv[2],
   inputStream = filename ? fs.createReadStream(filename) : process.stdin,
-  tokenizer = new Tokenizer;
+  tokenizer = new Tokenizer,
+  parser = new Parser({tokenizer: tokenizer}),
+  followSets = {},
+  currentFollow = null;
 
-tokenizer.on('error', function(err, next) {
+tokenizer.on('error', function(err) {
   console.error('tokenizer:error:', err);
-  next();
+  process.exit(1);
 });
 
 tokenizer.on('data', function(token) {
   if (token.type !== 'ws')
     console.log(token.toString());
+  if (!currentFollow) {
+    currentFollow = token;
+  } else {
+    followSets[currentFollow.type] = _.union(followSets[currentFollow.type] || [], [token.type]);
+    currentFollow = null;
+  }
+
 });
 
-inputStream.on('error', function(err, next) {
+tokenizer.on('end', function() {
+  console.log(tokenizer.toString());
+  console.dir(followSets);
+  console.log(util.inspect(parser.treeRoot, {colors: true, depth: null}));
+  parser.getErrors().forEach(function(error) {
+    console.log(error.message);
+  });
+});
+
+
+inputStream.on('error', function(err) {
   console.error('inputStream:error:', err);
-  next();
 });
 
 inputStream.pipe(tokenizer);
