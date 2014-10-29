91b325ebb9bf7f055b5da0a18c2d9e4f9f5a8cd4
Christopher Broome <ChrisBroome7@gmail.com>
2014-10-29 14:56:11 -0400

    Add some error handling to the parser

--- index.js
+++ index.js
@@ -2,15 +2,12 @@
 var
   util = require('util'),
   fs = require('fs'),
-  _ = require('lodash'),
   Tokenizer = require('./lib/tokenizer'),
   Parser = require('./lib/parser'),
   filename = process.argv[2],
   inputStream = filename ? fs.createReadStream(filename) : process.stdin,
   tokenizer = new Tokenizer,
-  parser = new Parser({tokenizer: tokenizer}),
-  followSets = {},
-  currentFollow = null;
+  parser = new Parser({tokenizer: tokenizer});
 
 tokenizer.on('error', function(err) {
   console.error('tokenizer:error:', err);
@@ -18,26 +15,18 @@ tokenizer.on('error', function(err) {
 });
 
 tokenizer.on('data', function(token) {
-  if (token.type !== 'ws')
-    console.log(token.toString());
-  if (!currentFollow) {
-    currentFollow = token;
-  } else {
-    followSets[currentFollow.type] = _.union(followSets[currentFollow.type] || [], [token.type]);
-    currentFollow = null;
-  }
-
+  if (token.type !== 'ws') console.log(token.toString());
 });
 
 tokenizer.on('end', function() {
+  var errors = parser.getErrors();
   console.log(tokenizer.toString());
-  console.dir(followSets);
   console.log(util.inspect(parser.treeRoot, {colors: true, depth: null}));
-  parser.getErrors().forEach(function(error) {
-    console.log(error.message);
+  console.log('Parsed ' + parser._tokenPointer + ' tokens. ' + errors.length + ' errors.');
+  errors.forEach(function(error) {
+    console.error('Error:', error.message);
   });
 });
-
 
 inputStream.on('error', function(err) {
   console.error('inputStream:error:', err);
