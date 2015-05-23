67dab3bd16e7ee5698ddb7bf17f988b823d8f2f9
Christopher Broome <ChrisBroome7@gmail.com>
2015-05-23 04:58:55 -0400

    Use lexit to get tokens

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-'use strict';
 var
   util = require('util'),
   fs = require('fs'),
-  Tokenizer = require('./lib/tokenizer'),
-  Parser = require('./lib/recursive-descent-parser'),
-  filename = process.argv[2],
-  inputStream = filename ? fs.createReadStream(filename) : process.stdin,
-  tokenizer = new Tokenizer(require('./lib/tokens/matchers'), require('./lib/token-factory')),
-  parser = new Parser({tokenizer: tokenizer});
+  lexit = require('lexit'),
+  Parser = require('./lib/recursive-descent-parser');
 
-tokenizer.on('error', function(err) {
-  console.error('tokenizer:error:', err);
-  process.exit(1);
-});
+(function main(){
 
-tokenizer.on('data', function(token) {
-  if (token.type !== 'ws') console.log(token.toString());
-});
+  var
+    matchers = require('./lib/tokens/matchers'),
+    tokenFactory = require('./lib/token-factory'),
+    tokenizer = getTokenizer(matchers, tokenFactory),
+    parser = new Parser({tokenizer: tokenizer}),
+    inputStream = getInputStream();
 
-tokenizer.on('end', function() {
-  var errors = parser.getErrors();
-  console.log(tokenizer.toString());
-  console.log(util.inspect(parser.treeRoot, {colors: true, depth: null}));
-  console.log('Parsed ' + parser._tokenPointer + ' tokens. ' + errors.length + ' errors.');
-  errors.forEach(function(error) {
-    console.error('Error:', error.message);
+  tokenizer.on('end', function() {
+    var errors = parser.getErrors();
+    console.log(tokenizer.toString());
+    console.log(util.inspect(parser.treeRoot, {colors: true, depth: null}));
+    console.log('Parsed ' + parser._tokenPointer + ' tokens. ' + errors.length + ' errors.');
+    errors.forEach(function(error) {
+      console.error('Error:', error.message);
+    });
   });
-});
 
-inputStream.on('error', function(err) {
-  console.error('inputStream:error:', err);
-});
+  inputStream.pipe(tokenizer);
 
-inputStream.pipe(tokenizer);
+}());
+
+function getInputStream() {
+  var
+    filename = process.argv[2],
+    inputStream = filename ? fs.createReadStream(filename) : process.stdin;
+
+  inputStream.on('error', function(err) {
+    console.error('inputStream:error:', err);
+  });
+
+  return inputStream;
+}
+
+
+function getTokenizer(matchers, tokenFactory) {
+  var tokenizer = new lexit.Tokenizer(matchers, tokenFactory);
+
+  tokenizer.on('error', function(err) {
+    console.error('tokenizer:error:', err);
+    process.exit(1);
+  });
+
+  tokenizer.on('data', function(token) {
+    if (token.type !== 'ws') console.log(token.toString());
+  });
+
+
+  return tokenizer;
+}
